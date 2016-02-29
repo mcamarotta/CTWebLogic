6fd1ebf0d9a9d0f6f03c7bbedea87825fac3aced
Michel Camarotta <mcamarotta@gmail.com>
2016-02-29 12:21:43 -0300

    no message

--- src/main/java/datatypes/DataCombination.java
+++ src/main/java/datatypes/DataCombination.java
@@ -49,16 +49,16 @@ public class DataCombination {
 		return Arrays.equals(this.combination.toArray(),otherObj.combination.toArray());
 	}
 
-//	@Override
-//	public String toString() {
-//		
-//		String res = "DataCombination [ ";
-//		for (String string : combination) {
-//			res=res+string+", ";
-//		}
-//		res=res+"]";
-//		return res;
-//	}
+	@Override
+	public String toString() {
+		
+		String res = "DataCombination [ ";
+		for (String string : combination) {
+			res=res+string+", ";
+		}
+		res=res+"]";
+		return res;
+	}
 	
 	
 	
